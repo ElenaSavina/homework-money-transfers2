5227d3c616779f77c4406e4ec469147300e6395a
Elena Savina <savina1289@yandex.ru>
2022-06-25 04:00:25 +0300

    Added function "calculateTransferFee"

--- src/main/kotlin/ru/netology/transfer-fee.kt
+++ src/main/kotlin/ru/netology/transfer-fee.kt
@@ -1,2 +1,40 @@
 package ru.netology
 
+import kotlin.math.roundToInt
+
+const val MIN_TRANSFER_FEE = 35_00
+const val EXTRA_TRANSFER_FEE = 20_00
+const val LIMIT_TRANSFER_FEE = 75_000_00
+const val STANDARD_PERCENT_FEE = 0.0075
+const val LIMIT_PERCENT_FEE = 0.006
+
+enum class TypeAccount {
+    Mastercard, Maestro, Visa, Mir, VKPay
+}
+
+fun main() {
+    val amountTransfer = 5000_00
+    val amountTransfersMonth = 80_000_00
+    val typeAccount = TypeAccount.Mastercard
+
+    val result = calculateTransferFee(amountTransfer, amountTransfersMonth, typeAccount)
+    println(result)
+}
+
+fun calculateTransferFee(
+    amountTransfer: Int,
+    amountTransfersMonth: Int = 0,
+    typeAccount: TypeAccount = TypeAccount.VKPay
+): Int {
+    return when (typeAccount) {
+        TypeAccount.Maestro, TypeAccount.Mastercard -> {
+            if (amountTransfersMonth < LIMIT_TRANSFER_FEE) 0
+            else (amountTransfer * LIMIT_PERCENT_FEE + EXTRA_TRANSFER_FEE).roundToInt()
+        }
+        TypeAccount.Visa, TypeAccount.Mir -> {
+            val transferFee = (amountTransfer * STANDARD_PERCENT_FEE).roundToInt()
+            if (transferFee > MIN_TRANSFER_FEE) transferFee else MIN_TRANSFER_FEE
+        }
+        TypeAccount.VKPay -> 0
+    }
+}
